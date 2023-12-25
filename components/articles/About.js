87aa838eeb7177eac8928f0218308d2ee6be284a
karkir0003 <karkir0003@gmail.com>
2023-12-25 13:19:02 -0500

    next/image migration

--- components/articles/About.js
+++ components/articles/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <>
             <h2 className="major">About</h2>
-            <span className="image main"><Image src="/static/images/pic03.jpg" alt="" /></span>
+            <span className="image main"><Image src="/static/images/pic03.jpg" height={500} width={500} alt="" /></span>
             <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
         </>
     )
--- components/articles/Work.js
+++ components/articles/Work.js
@@ -5,7 +5,7 @@ const Work = () => {
     return (
         <>
             <h2 className="major">Work</h2>
-            <span className="image main"><Image src="/static/images/pic02.jpg" alt="" /></span>
+            <span className="image main"><Image src="/static/images/pic02.jpg" height={500} width={500} alt="" /></span>
             <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
             <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
         </>
